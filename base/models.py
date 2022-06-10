26e1ab01af06b39e6d461780afa130086eefef3e
MatteoDrso <67522359+MatteoDrso@users.noreply.github.com>
2022-06-11 01:45:29 +0200

    created Comment model
    (incomplete)

--- base/models.py
+++ base/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+from django.contrib.auth import settings
+
+class Comment(models.Model):
+	#parent = models.ForeignKey(Comment) #does that make sense?
+	publisher = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null = True,
+    ) #not sure if this works properly. source: https://docs.djangoproject.com/en/1.10/topics/auth/customizing/#referencing-the-user-model
+	title = models.CharField(max_length=200)
+	message = models.TextField()
+	publication_date = models.DateTimeField()
+
+	def __str__(self):
+		return self.title, self.message()
