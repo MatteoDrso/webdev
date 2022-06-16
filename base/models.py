0528c815f8c1dd75566df231904f483a1388bd0e
MatteoDrso <67522359+MatteoDrso@users.noreply.github.com>
2022-06-16 18:15:53 +0200

    finished Comment model
    Comment model should be completely functional.

--- base/models.py
+++ base/models.py
@@ -4,10 +4,14 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth import settings
 
 class Comment(models.Model):
-	#parent = models.ForeignKey(Comment) #does that make sense?
+	parent = models.ForeignKey(
+		'self',
+		on_delete = models.CASCADE, #does that make sense?
+		null = True,
+		) 
 	publisher = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
+        on_delete = models.CASCADE,
         null = True,
     ) #not sure if this works properly. source: https://docs.djangoproject.com/en/1.10/topics/auth/customizing/#referencing-the-user-model
 	title = models.CharField(max_length=200)
@@ -15,4 +19,4 @@ class Comment(models.Model):
 	publication_date = models.DateTimeField()
 
 	def __str__(self):
-		return self.title, self.message()
+		return self.title
