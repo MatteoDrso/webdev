22da8b667e5747b66d5dc49dd756ce48b168c5bb
MatteoDrso <67522359+MatteoDrso@users.noreply.github.com>
2022-06-26 21:01:23 +0200

    Revert "renamed the url ref"
    This reverts commit 25201b4445c5a18740646b073f152a907d79d792.

--- base/urls.py
+++ base/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('post/create', views.create_post, name='create_post'),
-    path('post/reply/<int:id>', views.reply_post, name='reply_post'),
     path('post/view/<int:id>', views.view_post, name='view_post'),
     path('post/delete/<int:id>', views.delete_post, name='delete_post'),
     path('post/edit/<int:id>', views.edit_post, name='edit_post'),
