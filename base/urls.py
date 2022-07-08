db8be08fceb8bdaa93ebe9f486998c8d980bc7e4
MatteoDrso <67522359+MatteoDrso@users.noreply.github.com>
2022-07-08 18:13:20 +0200

    added post/reply/ url

--- base/urls.py
+++ base/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('post/create', views.create_post, name='create_post'),
     path('post/view/<int:id>', views.view_post, name='view_post'),
+    path('post/reply/<int:id>', views.reply_post, name='reply_post'),
     path('post/delete/<int:id>', views.delete_post, name='delete_post'),
     path('post/edit/<int:id>', views.edit_post, name='edit_post'),
     path('user/view/<int:id>', views.user_profile, name='user_profile'),
